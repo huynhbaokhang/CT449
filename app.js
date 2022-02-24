202b21ef99bdc218c9a11fc3ec6d6d428bb58590
huynhbaokhang <73773630+kagnag@users.noreply.github.com>
2022-02-24 12:20:00 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const { BadRequestError, errorHandler } = require("./app/errors");
+
 const app = express();
 app.use(cors());
 
@@ -14,5 +16,19 @@ app.get("/", (req, res) => {
 
 const setupContactRoutes = require("./app/routes/contact.routes");
 setupContactRoutes(app);
+
+// handle 404 response
+app.use((req, res, next) => {
+  // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+  // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+  next(new BadRequestError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+  // Middleware xử lý lỗi tập trung.
+  // Trong các đoạn code xử lý ở các route, gọi next(error)
+  // sẽ chuyển về middleware xử lý lỗi này
+  errorHandler.handleError(error, res);
+});
 
 module.exports = app;
